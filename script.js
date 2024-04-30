8617aafc646af6825b45c562ba1e597d6800d58f
Gabriel Gingras <gabgingras@yahoo.com>
2024-04-30 18:42:23 -0400

    Fix no button

--- script.js
+++ script.js
@@ -29,18 +29,16 @@ function moveNonButton() {
     button.style.top = newY + 'px';
 }
 
-
-window.addEventListener('resize', moveNonButton);
-
-document.getElementById('nonButton').addEventListener('mouseenter', moveNonButton);
-
+function initialize() {
+    document.getElementById('nonButton').addEventListener('mouseenter', moveNonButton);
+    window.addEventListener('resize', moveNonButton);
+    document.getElementById('nonButton').addEventListener('mouseenter', function() {
+        img.src = 'img/cats-sad.gif';
+    });
+    document.getElementById('ouiButton').addEventListener('mouseenter', function() {
+        img.src = 'img/love-cat.gif';
+    });
+}
 
 const img = document.querySelector('img');
-
-document.getElementById('nonButton').addEventListener('mouseenter', function() {
-    img.src = 'img/cats-sad.gif';
-});
-
-document.getElementById('ouiButton').addEventListener('mouseenter', function() {
-    img.src = 'img/love-cat.gif';
-});
+initialize();
